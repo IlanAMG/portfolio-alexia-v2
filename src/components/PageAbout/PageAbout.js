d2ccfc80f767cfc40cf3a7f71c9b640bfd00cbb7
Ilan Amzallag <59542957+IlanAMG@users.noreply.github.com>
2020-06-16 22:26:28 +0200

    v0.1.3 aboutpage

--- src/components/PageAbout/PageAbout.js
+++ src/components/PageAbout/PageAbout.js
@@ -1,15 +1,64 @@
 import React, { useContext } from 'react'
+import { BsEnvelope } from 'react-icons/bs';
+import { AiOutlineInstagram } from 'react-icons/ai';
 
 import StyledPageAbout from './StyledPageAbout';
 import Context from '../../utils/context';
 
 export const PageAbout = () => {
     const { openNavTransiFinish } = useContext(Context)
-    
+
     return (
         !openNavTransiFinish &&
         <StyledPageAbout>
-            FUTURE PAGE ABOUT
+            <div className='container-passion'>
+                <img className='profil1' alt='profil1' src={'https://i.imgur.com/ghtp1Iu.jpg'} />
+                <div className='wrapper-txt'>
+                    <h4><small>"</small>PASSION<small>"</small></h4>
+                    <p>Passionate about art I have decided at a young age to made this passion my job.<br /><br />
+                    I am always looking for oppotunities to practice my art, I wish to bring up my arts in the make-up industry.
+                    </p>
+                    <div className='circle' />
+                </div>
+            </div>
+            <div className='container-major'>
+                <div className='wrapper-txt'>
+                    <h4><small>"</small>MAJOR<small>"</small></h4>
+                    <p>After getting a highschool degree in litterature, I have graduated major of promotion, expert make-up artist degree front the Conservatoire Du Maquillage in Paris.<br/><br/>
+                        I can create fashion, beauty, television and theater Make-up, Also FX Make-up and even face prothesis.
+                    </p>
+                    <div className='circle' />
+                </div>
+                <img className='profil2' alt='profil2' src={'https://i.imgur.com/Ne5YVF9.jpg'} />
+            </div>
+            <div className='container-skills'>
+                <img className='profil3' alt='profil3' src={'https://i.imgur.com/Eqt4yUW.jpg'} />
+                <div className='wrapper-txt'>
+                    <h4><small>"</small>SKILLS<small>"</small></h4>
+                    <p>I have many skills, My experience goes from numerous photo shooting, to musicals passing by shortmovies and dancing shows. 
+                    </p>
+                    <div className='circle' />
+                </div>
+            </div>
+            <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+            <div className='container-insta'>
+                <div className='wrapper-contact'>
+                    <BsEnvelope />
+                    <small>alexiaamzlg@gmail.com</small>
+                </div>
+                <iframe title='insta' src="https://cdn.lightwidget.com/widgets/52b2fbe5880d5c71b4e2dcc3d08931c2.html" scrolling="no" allowtransparency="true" className="lightwidget-widget"></iframe>
+                <div className='wrapper-contact'>
+                    <AiOutlineInstagram />
+                    <small>alexiaamzlg@gmail.com</small>
+                </div>
+                <div className='circle' />
+            </div>
+            <footer>
+                <div className='circle' />
+                <p>
+                    designed by dev’trotter<br/><br/>2020
+                </p>
+            </footer>
         </StyledPageAbout>
     )
 }
--- src/components/PageAbout/StyledPageAbout.js
+++ src/components/PageAbout/StyledPageAbout.js
@@ -2,15 +2,226 @@ import styled from 'styled-components'
 
 const StyledPageAbout = styled.section`
     width: 100vw;
-    height: 100vh;
-    position: fixed;
+    min-height: 100vh;
     display: flex;
+    flex-direction: column;
     align-items: center;
-    justify-content: center;
     background: linear-gradient(180deg, #E9D6FF 0%, #F1E5FF 45.79%, #F1E5FF 67.69%, #FFFFFF 127.41%);
-    overflow-Y: hidden;
-    overflow-X: scroll;
     transition: 1000ms;
+    overflow: hidden;
+        .container-passion {
+            margin-top: 120px;
+            padding-left: 65px;
+            padding-right: 65px;
+            max-height: 650px;
+            width: 100vw;
+            display: flex; 
+            z-index: 1;
+        }
+        .container-skills {
+            margin-top: 13vw;
+            padding-left: 65px;
+            padding-right: 65px;
+            max-height: 650px;
+            min-height: 34vw;
+            width: 100vw;
+            display: flex; 
+            justify-content: flex-end;
+            z-index: 1;
+            position: relative;
+        }
+        .container-major {
+            margin-top: 6vw;
+            padding-left: 65px;
+            padding-right: 65px;
+            max-height: 650px;
+            width: 100vw;
+            display: flex; 
+            z-index: 1;
+            position: relative;
+        }
+        .container-passion .wrapper-txt {
+            padding-left: 8px;
+        }
+        .container-major .wrapper-txt {
+            text-align: right;
+            width: calc(62.30% - 65px);
+        }
+        .container-skills .wrapper-txt {
+            padding-top: 50px;
+            width: calc(62.30% - 65px);
+        }
+
+        .container-major .profil2 {
+            width: 37.70%;
+            z-index: 1;
+            position: absolute;
+            bottom: -11.5vw;
+            right: 65px;
+        }
+
+        .container-passion .profil1 {
+            width: 37.70%;
+            z-index: 1;
+        }
+        .container-skills .profil3 {
+            width: 37.70%;
+            z-index: 1;
+            position: absolute;
+            top: -10vw;
+            left: 65px;
+        }
+
+        .wrapper-txt {
+            position: relative;
+        }
+
+        .wrapper-txt .circle {
+            border-radius: 50%;
+            transform: rotate(17.31deg);
+            position: absolute;
+            width: 26.6vw;
+            height: 26.6vw;
+            min-width: 300px;
+            min-height: 300px;
+            z-index: -1;
+        }
+        .container-passion .circle {
+            left: -5.98vw;
+            top: 0;
+            background: linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%);
+        }
+        .container-major .circle {
+            right: -24%;
+            top: -20%;
+            background: linear-gradient(180deg, #D1B4FF 0%, rgba(255, 255, 255, 0) 100%);
+            transform: rotate(-150deg);
+        }
+        .container-skills .circle {
+            left: -15%;
+            top: -8%;
+            background: linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%);
+            transform: rotate(105deg);
+        }
+
+        h4 {
+            font-size: 100px;
+            line-height: 100px;
+            color: #121212;
+            text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+            z-index: 1;
+        }
+        .container-passion p, .container-skills p {
+            font-size: 32px;
+            width: 100%;
+            line-height: 32px;
+            letter-spacing: 0.05em;
+            color: #000000;
+            text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+            white-space:wrap;
+            padding-left: 2vw;
+            z-index: 1;
+        }
+        .container-major p {
+            font-size: 32px;
+            width: 100%;
+            line-height: 32px;
+            letter-spacing: 0.05em;
+            color: #000000;
+            text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+            white-space:wrap;
+            padding-right: 2vw;
+            z-index: 1;
+        }
+        small {
+            color: #D1B4FF;
+            z-index: 1;
+        }
+
+        footer {
+            position: relative;
+            height: 120px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 100%;
+            z-index: 1;
+        }
+
+        footer .circle {
+            border-radius: 50%;
+            position: absolute;
+            width: 26.6vw;
+            height: 26.6vw;
+            min-width: 300px;
+            min-height: 300px;
+            left: -6vw;
+            bottom: -10vw;
+            z-index: -1;
+            background: linear-gradient(180deg, #D1B4FF 0%, rgba(255, 255, 255, 0) 100%);
+            transform: rotate(120.82deg);
+        }
+
+        footer p {
+            font-size: 14px;
+            line-height: 14px;
+            text-align: center;
+            color: #000000;
+        }
+
+    .container-insta {
+        width: 80%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        position: relative;
+        z-index: 2;
+    }
+
+    .container-insta .wrapper-contact {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+    .container-insta .wrapper-contact svg {
+        width: 40px;
+        height: 40px;
+        margin-bottom: 10px;
+    }
+    .container-insta .wrapper-contact small {
+        color: #121212;
+        font-size: 24px;
+        line-height: 24px;
+        text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    }
+
+    .container-insta .circle {
+        border-radius: 50%;
+        position: absolute;
+        width: 26.6vw;
+        height: 26.6vw;
+        min-width: 300px;
+        min-height: 300px;
+        right: -6%;
+        top: -40%;
+        z-index: 1;
+        background: linear-gradient(180deg, #D1B4FF 0%, rgba(255, 255, 255, 0) 100%);
+        transform: rotate(-60deg);
+    }
+
+    iframe {
+        height: 400px;
+        width: 400px;
+        z-index: 2;
+    }
+
+    .lightwidget-widget {
+        border: none;
+        height: 400px;
+        width: 400px;
+        z-index: 2;
+    }
 `
 
 export default StyledPageAbout
