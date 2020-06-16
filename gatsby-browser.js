9d88e046a7210e2a8a1d9a9d6a4b3b5da317d405
Ilan Amzallag <59542957+IlanAMG@users.noreply.github.com>
2020-06-17 00:15:47 +0200

    v0.1.4 aboutpage

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,15 +2,15 @@
 import React from 'react'
 import GlobalContextProvider from './src/utils/GlobalContextProvider';
 
-export const onLoad = () => {
-      if (window) {
-        window.onload = () => { 
-          return true ;
-        }
-      } else {
-        return false;
-      }
-    }
+// export const onLoad = () => {
+//       if (window) {
+//         window.onload = () => { 
+//           return true ;
+//         }
+//       } else {
+//         return false;
+//       }
+//     }
 
 export const wrapRootElement = ({ element }) => {
   return (
